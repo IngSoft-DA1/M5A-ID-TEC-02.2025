de327450dc79ef125d97395242964128b43ea4b4
FACULTADES\alumnoFI <asdsad@gmail.com>
2025-08-28 11:07:53 -0300

    ejercicio clase completado

---
+++ Console/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Domain;
+
+
+
+Console.WriteLine("Ingrese datos del usuario");
+Console.Write("Ingrese nombre: ");
+string nombre = Console.ReadLine();
+Console.Write("Ingrese fecha de nacimiento: ");
+string date = Console.ReadLine();
+
+if (DateTime.TryParse(date, out DateTime validDate))
+{
+
+    Persona persona = new Persona
+    {
+        Name = nombre,
+        BirthDate = validDate,
+    };
+    
+    Console.WriteLine("El usuario tiene nombre {0}", persona.Name);
+    Console.WriteLine($"El usuario tiene edad {persona.GetAge()}");
+    Console.WriteLine("El usuario nacio el dia {0}", persona.DayOfTheWeekBirth());
+    
+}
---
+++ Domain/Persona.cs
@@ -0,0 +1,20 @@
+﻿using System.Globalization;
+
+namespace Domain;
+
+public class Persona
+{
+    public string Name { get; set; }
+    public DateTime BirthDate { get; set; }
+
+    public int GetAge()
+    {
+        return DateTime.Now.Year - BirthDate.Year;
+    }
+
+    public string DayOfTheWeekBirth()
+    {
+        return this.BirthDate.ToString("dddd", new CultureInfo("es-ES")); // Convertir a espanol
+        // return this.BirthDate.DayOfWeek.ToString(); // Por defecto en ingles
+    }
+}
